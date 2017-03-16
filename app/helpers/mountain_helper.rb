dc9835248ca0746015817de4cb63d518de274b74
Tim Ross <tim.ross@90seconds.tv>
2017-03-16 20:28:35 +1300

    Move mountain into db directory

--- app/helpers/mountain_helper.rb
+++ app/helpers/mountain_helper.rb
@@ -1,7 +1,7 @@
 module MountainHelper
 
   def paint_3peak_mountain(percentage)
-    path = Rails.root.join('app', 'assets', 'html', 'mountain.html')
+    path = Rails.root.join("db", "mountain.html")
     contents = File.open(path, "rb").read
     lines = contents.split("\n")
     # Display a certain percentage of the above lines based on the snowy_mountains input
